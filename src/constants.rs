4784a8b9eaab6c2dd127d00c18c6bb5d2787a082
Ludi Rehak <ludi317@gmail.com>
2025-06-10 22:53:01 -0700

    Improve the dino sprite from a series of 2 images to a series of 10 images ripped from reddit (https://www.reddit.com/r/Dinosaurs/comments/1fx04sa/working_on_a_new_trex_sprite/). Stop the dino from moving its legs when in mid-air.

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,6 @@
 use bevy::math::Vec2;
 
-
-pub const PLAYER_SIZE: Vec2 = Vec2::new(87.0, 94.0);
+const SCALE: f32 = 0.15;
+pub const PLAYER_SIZE: Vec2 = Vec2::new(1328.0 * SCALE, 768.0 * SCALE);
 pub const GROUND_LEVEL: f32 = -300.0;
 pub const INITIAL_HEALTH: usize = 99;
