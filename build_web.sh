72be6bc3ab177b8ce07e1fcd87f4dfb4c9810315
Ludi Rehak <ludi317@gmail.com>
2025-06-20 14:10:11 -0700

    Improve web build script

--- build_web.sh
+++ build_web.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Build for web
 cargo build --profile web --target wasm32-unknown-unknown
@@ -6,8 +7,5 @@ cargo build --profile web --target wasm32-unknown-unknown
 # Generate JS bindings
 wasm-bindgen --target web --out-dir static --no-typescript \
     target/wasm32-unknown-unknown/web/my_bevy_game.wasm
-
-# Copy assets to static directory (if you have any)
-cp -r assets static/ 2>/dev/null || :
 
 echo "Build complete! Files are in static/"
